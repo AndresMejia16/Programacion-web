89b90cab38fdf433f2bfa75bb86d7e780dd0bf9c
AndresMejia16 <44811528+AndresMejia16@users.noreply.github.com>
2021-11-26 16:45:59 -0500

    Add files via upload

---
+++ Proyecto web/js/capturar.js
@@ -0,0 +1,14 @@
+
+//Definimos el botón para escuchar su click, y también el contenedor del canvas
+const $boton = document.querySelector("#btnCapturar"), // El botón que desencadena
+  $objetivo = document.querySelector("#container"), // A qué le tomamos la foto
+  $contenedorCanvas = document.querySelector("#contenedorCanvas"); // En dónde ponemos el elemento canvas
+
+// Agregar el listener al botón
+$boton.addEventListener("click", () => {
+  html2canvas($objetivo) // Llamar a html2canvas y pasarle el elemento
+    .then(canvas => {
+      // Cuando se resuelva la promesa traerá el canvas
+      $contenedorCanvas.appendChild(canvas); // Lo agregamos como hijo del div
+    });
+});
